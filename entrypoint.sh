b1027dfb5e98d6389fa19e9278fe84f80bc2a25e
Eric Chapman <echapman@bunnings.com.au>
2023-08-21 12:23:10 +0800

    ci: remove review dog

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,9 @@
 
 cd "${GITHUB_WORKSPACE}" || exit 1
 
-export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-
 detect-secrets --version
 
 git config --global --add safe.directory /github/workspace
 
 detect-secrets scan ${INPUT_DETECT_SECRETS_FLAGS} ${INPUT_WORKDIR} \
-    | baseline2rdf -json_filename="${INPUT_JSON_FILENAME}" \
-    | reviewdog -f=rdjson \
-        -name="${INPUT_NAME:-detect-secrets}" \
-        -filter-mode="${INPUT_FILTER_MODE:-added}" \
-        -reporter="${INPUT_REPORTER:-github-pr-check}" \
-        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-        -level="${INPUT_LEVEL}" \
-        ${INPUT_REVIEWDOG_FLAGS}
+    | baseline2rdf -json_filename="${INPUT_JSON_FILENAME}"
